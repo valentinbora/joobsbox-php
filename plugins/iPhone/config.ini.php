6d36da690a495d3cd0e8e3cb53058f65bb511e23
Manuel R. Ciosici <manuel@manuel-ubuntulaptop.(none)>
2009-07-13 20:04:25 +0300

    iphone support almost complete

--- plugins/iPhone/config.ini.php
+++ plugins/iPhone/config.ini.php
@@ -1,3 +1,4 @@
 [main]
 title_ro			=	iPhone
 title_en			=	iPhone
+image				=	images/iphone_24.png
--- plugins/iPhone/iPhone.php
+++ plugins/iPhone/iPhone.php
@@ -11,6 +11,8 @@ class iPhone extends Joobsbox_Plugin_Base {
 	} */
 	public function filter_head_html(){
 		$meta = '<meta name = "viewport" content = "width = device-width user-scalable = no">';
+		$meta .= '<link rel="apple-touch-startup-image" href="/plugins/iPhone/startup.png">';
+		
 		return array($meta);
 	}
 }
