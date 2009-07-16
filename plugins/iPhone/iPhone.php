3ec47152c4fd3fcc54c39b3940fc525748c30998
Manuel R. Ciosici <manuel@manuel-ubuntulaptop.(none)>
2009-07-16 17:05:43 +0300

    fixed final issue with iPhone css

--- plugins/iPhone/iPhone.php
+++ plugins/iPhone/iPhone.php
@@ -8,7 +8,7 @@ class iPhone extends Joobsbox_Plugin_Base {
     $this->_helper->headMeta()->prependName('viewport', 'width = device-width user-scalable = no');
     //<meta name = "viewport" content = "width = device-width user-scalable = no">
     
-    $this->_helper->headLink()->appendStylesheet($this->baseUrl . 'plugins/iPhone/css/iphone.css', "only screen and (max-device-width: 480px)");
+    $this->_helper->headLink()->appendStylesheet($this->baseUrl . '/plugins/iPhone/css/iphone.css', "only screen and (max-device-width: 480px)");
     /*<link media="only screen and (max-device-width: 480px)" href="<?php echo $this->baseUrl ?>/plugins/iPhone/css/iphone.css" type= "text/css" rel="stylesheet"> */
     
     $this->_helper->headLink()->headLink(array('rel' => 'apple-touch-startup-image',
