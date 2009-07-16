fad5f311a1d641bfe81c49942c625e7b59a8359a
Manuel R. Ciosici <manuel@manuel-ubuntulaptop.(none)>
2009-07-16 16:50:14 +0300

    iPhone plugin now loads css and js the correct way

--- plugins/iPhone/iPhone.php
+++ plugins/iPhone/iPhone.php
@@ -4,11 +4,18 @@ class iPhone extends Joobsbox_Plugin_Base {
    
   }
   public function filter_head_html(){
-    $meta = '<meta name = "viewport" content = "width = device-width user-scalable = no">';
-    $meta .= '<link rel="apple-touch-startup-image" href="/plugins/iPhone/images/startup.png">';
-   // $this->view->headScript()->appendFile($this->view->baseUrl() . 'default/public_html/js/prototype.js');
-    $this->_helper->headLink()->appendStylesheet($this->baseUrl . 'plugins/iPhone/css/iphone.css');  
-    /*<link media="only screen and (max-device-width: 480px)" href="<?php echo $this->baseUrl ?>/plugins/iPhone/css/iphone.css" type= "text/css" rel="stylesheet">
-    */return array($meta);
+    
+    $this->_helper->headMeta()->prependName('viewport', 'width = device-width user-scalable = no');
+    //<meta name = "viewport" content = "width = device-width user-scalable = no">
+    
+    $this->_helper->headLink()->appendStylesheet($this->baseUrl . 'plugins/iPhone/css/iphone.css', "only screen and (max-device-width: 480px)");
+    /*<link media="only screen and (max-device-width: 480px)" href="<?php echo $this->baseUrl ?>/plugins/iPhone/css/iphone.css" type= "text/css" rel="stylesheet"> */
+    
+    $this->_helper->headLink()->headLink(array('rel' => 'apple-touch-startup-image',
+                                  'href' => $this->baseUrl . '/plugins/iPhone/images/startup.png'),
+                                  'PREPEND');
+    
+    $this->_helper->headScript()->appendFile($this->baseUrl . '/plugins/iPhone/js/iphone.js', 'text/javascript');
+    return false;
   }
 }
