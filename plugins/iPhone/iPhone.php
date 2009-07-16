e5503cf293ce642f1dc23c89ff327bc80cd72914
Manuel R. Ciosici <manuel@manuel-ubuntulaptop.(none)>
2009-07-16 13:39:55 +0300

    trying to get up to date

--- plugins/iPhone/iPhone.php
+++ plugins/iPhone/iPhone.php
@@ -1,18 +1,19 @@
 <?php
 class iPhone extends Joobsbox_Plugin_Base {
-	public function init() {
-	
-	}
-
-/*	function event_app_init () {
-//		dd("Shiiiit");
-
-		configureTheme(APPLICATION_THEME, 'iphoneindex');
-	} */
-	public function filter_head_html(){
-		$meta = '<meta name = "viewport" content = "width = device-width user-scalable = no">';
-		$meta .= '<link rel="apple-touch-startup-image" href="/plugins/iPhone/images/startup.png">';
-		
-		return array($meta);
-	}
+  public function init() {
+    
+  }
+ 
+/*  function event_app_init () {
+//    dd("Shiiiit");
+ 
+    configureTheme(APPLICATION_THEME, 'iphoneindex');
+  } */
+  public function filter_head_html(){
+    $meta = '<meta name = "viewport" content = "width = device-width user-scalable = no">';
+    $meta .= '<link rel="apple-touch-startup-image" href="/plugins/iPhone/images/startup.png">';
+    
+    return array($meta);
+  }
+  
 }
