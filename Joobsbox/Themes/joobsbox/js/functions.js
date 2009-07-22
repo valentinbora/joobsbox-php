22922a52f5113f665b84f1222e3e0d21f0897f86
Valentin Bora <valentinbora@valentin-boras-macbook-pro.local>
2009-07-23 01:50:52 +0300

    Modified rounded corners scripts

--- Joobsbox/Themes/joobsbox/js/functions.js
+++ Joobsbox/Themes/joobsbox/js/functions.js
@@ -1,10 +1,2 @@
-// Contains global site functions (mainly helpers)
-
-// Translate any string using the global site translation
-function translate(string) {
-	if(translateHash[string]) {
-		return translateHash[string];
-	} else {
-		return string;
-	}
-}
+$("div.box, div.plain").corner("round top");
+$("div.outer").corner("round top");
