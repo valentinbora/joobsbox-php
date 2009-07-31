3c8d87ffe79e8da28888de7a09a318151c122bbd
Valentin Bora <valentinbora@valentin-boras-macbook-pro.local>
2009-07-31 17:33:11 +0300

    Changed parser to htmLawed. More lightweight.

--- plugins/Xssandstuff/Xssandstuff.php
+++ plugins/Xssandstuff/Xssandstuff.php
@@ -2,15 +2,8 @@
 
 class Xssandstuff extends Joobsbox_Plugin_Base
 {
-	private $purifier;
-	
-	public function __construct() {
-		require_once dirname(__FILE__) . '/library/HTMLPurifier.includes.php';
-		$this->purifier = new HTMLPurifier();
-	}
-	
 	public function filter_purify_html($html) {
-	
-		return array($this->purifier->purify($html));
+	  require_once dirname(__FILE__) . '/library/htmLawed/htmLawed.php';
+		return array(htmLawed($html, array('safe'=>1, 'deny_attribute'=>'style')));
 	}
 }
